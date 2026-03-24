9b04d87e298424499cb023465a7fb52c1f8d032f
Ana Martinez <ccmx290@gmail.com>
2026-03-23 23:06:13 -0500

    Added form validation to the Donation form

--- js/form-validation.js
+++ js/form-validation.js
@@ -29,4 +29,39 @@ $("#contactForm").validate({
     }
 });
 
+$("#donateForm").validate({
+    rules: {
+        firstname: {
+            required: true,
+            minlength: 2
+        },
+        lastname: {
+            required: true,
+            minlength: 2
+        },
+        email: {
+            required: true
+        },
+        amount: {
+            required: true
+        },
+        message: {
+            required: true,
+            minlength: 15
+        }
+    },
+
+    messages: {
+        firstname: "Please enter your first name. Minimum of 2 characters required.",
+        lastname: "Please enter your last name. Minimum of 2 characters required.",
+        email: "Please enter a valid email address.",
+        amount: "Please select a donation amount.",
+        message: "Please enter a message with a minimum of 15 characters."
+    },
+
+    submitHandler: function(form) {
+        form.submit();
+    }
+});
+
 $("input", "textarea").addClass("error");
