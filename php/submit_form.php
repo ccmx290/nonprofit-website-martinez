7373a60d3f040f3fd6bf93f250186c979d2f71d9
Ana Martinez <ccmx290@gmail.com>
2026-04-30 16:08:44 -0500

    Updated db_connect_sample and submit_form pages;

--- php/submit_form.php
+++ php/submit_form.php
@@ -1,23 +1,39 @@
 <?php
 
-require('db_connect.php');
+require('db_connect_sample.php');
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-
-if (isset($_POST['submitBtn'])) {
-    echo "Submitted.";
+    echo "The form was submitted.";
 }
-
-if (isset($_POST['firstname']) && isset($_POST['lastname']) 
-&& isset($_POST['email']) && isset($_POST['message'])) {
-    echo $firstname;
-    echo $lastname;
-    echo $email;
-    echo $message;
+else {
+    echo "Nothing was submitted.";
+    exit();
 }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+
+        <title>Form Submission</title>
+    </head>
+    <body>
+        <h1>Form Submission</h1>
+
+        <div>
+            <p><span>First Name:</span> <?php echo htmlspecialchars($firstname); ?> </p>
+            <p><span>Last Name:</span> <?php echo htmlspecialchars($lastname); ?> </p>
+            <p><span>Email:</span> <?php echo htmlspecialchars($email); ?> </p>
+            <p><span>Message:</span> <?php echo htmlspecialchars($message); ?> </p>
+        </div>
+    </body>
+</html>
